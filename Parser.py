91f0effc2116eada1646c2de7332f7bab960cdfd
MarDeus <phillippemendoza@yahoo.com>
2019-04-11 20:12:35 -0400

    Add files via upload
    Looks all the way through dir/subdirs to find file and search through them using regular expressions.

---
+++ Parser.py
@@ -0,0 +1,35 @@
+import os
+import os.path
+import re
+
+
+def parse():                                                            # Function to search through dirs/subdirs for files
+    workingD = "<working directory>"
+    Specific_files = []
+    os.chdir(workingD)
+    for root, dirs, files in os.walk(workingD, topdown = True):         # Recursively searching parent dirs to pull out files
+        for name in files:
+            filepath = os.path.join(root, name)                         # Creates a file path for the file by conjoining the root with the name
+            Specific_files.append({"name": name, "filepath": filepath})     # Appends the name of file and path to a list
+    return Specific_files                                               # Returns the list
+
+
+def file_parse():                                               # Function to parse through the files contents using Regex
+    file_dict = parse()                     # Call back to the list
+    data_dict = []
+    overflow = []
+    for i in file_dict:
+        with open(i['filepath'], 'r') as search_file:
+            print(i['filepath'])                            # Prints out the file path being parsed. Totally unnecesary but it helps you see where the process is at and it looks cool as it runs.
+            contents = search_file.read()
+            data = re.findall(r"(.*?)", contents, re.I)                 # Regex to search through the file, case ignored
+            data_dict.append(data)                          # Appends the data found to a list in order to be split apart and check for originality.
+    with open('C:\Users\Phillippe\Documents\Programming\DDS\mhamill.txt', 'w') as document:
+        for item in str(suspicious_dict).split(","):
+            while item not in overflow:                         # Checks the data through a list in order to rid of duplicates
+                overflow.append(item)
+                print >> document, item             # Python 2.7 writes to a document or you can use the below
+                #document.write("%s\n" % item)          # Python3
+
+file_parse()
+
